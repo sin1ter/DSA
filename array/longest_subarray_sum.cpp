7ea733b8286eed5298b17f973e6b6cc63a8a47b0
Symon <its.symonbarua@gmail.com>
2023-10-24 07:20:38 +0600

    array

--- array/longest_subarray_sum.cpp
+++ array/longest_subarray_sum.cpp
@@ -18,21 +18,42 @@ using namespace std;
 // }
 
 int longestSubarray(int arr[], int n, int k) {
+    //when the array got negatives and zeros this is the optimal soluiton.
+    map<long long, int> prefixSum;
+    long long sum = 0;
     int maxlen = 0;
-    int sum = 0;
-    map<int, int> presumMap;
     for(int i = 0; i < n; i++) {
-        sum += arr[i];
+        sum += arr[i]; // calculating the prefix sum till index i
         if(sum == k) {
-            maxlen = max(maxlen, i + 1);
+            maxlen = max(maxlen, i + 1); // if the sum = k update the maxlen
         }
-        int rem = sum - k;
-        if(presumMap.find(rem) != presumMap.end()) {
-            int len = i - presumMap[rem];
+        long long rem = sum - k; // calculating the sum of remainging part x - k
+        if(prefixSum.find(rem) != prefixSum.end()) { // calculating the length and update maxlen
+            int len = i -  prefixSum[rem];
             maxlen = max(maxlen, len);
         }
-        if(presumMap.find(sum) == presumMap.end()) {
-            presumMap[sum] = i;
+        if(prefixSum.find(sum) == prefixSum.end()) { //Finally, update the map chekcing the conditions
+            prefixSum[sum] = i;
+        }
+    }
+    return maxlen;
+}
+
+int longestSubarray2pointer(int arr[], int n, int k) {
+    long long sum = arr[0];
+    int left = 0, right = 0;
+    int maxlen = 0;
+    while(right < n) {
+        while(left <= right and sum > k) {
+            sum -= arr[left];
+            left++;
+        }
+        if(sum == k) {
+            maxlen = max(maxlen, right -left + 1);
+        }
+        right++;
+        if(right < n) {
+            sum += arr[right];
         }
     }
     return maxlen;
@@ -45,6 +66,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    // cout << longest_subarray(arr, n, k) << '\n'; // For this time complexity is O(n^2) and space complexity is O(1)
-    cout << longestSubarray(arr, n, k);
+    // cout << longest_subarray(arr, n, k) << '\n'; // For this time complexity is O(n^2) and space complexity is O(1).
+    // cout << longestSubarray(arr, n, k); // for this time complexity is O(n*logn) and space complexity for this is O(n).
+    cout << longestSubarray2pointer(arr, n, k); // for this time complexity is O(2*n) and space complexity is O(1)
 }
