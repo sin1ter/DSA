b4093bfa4118cca06ad66dac17085ec79d348ea7
Symon <its.symonbarua@gmail.com>
2023-11-04 12:01:00 +0600

    array

---
+++ array/length _of_the_longest_subarray_with_zero_sum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longest_length(vector<int>& arr, int n) {
+    map<int, int> mpp;
+    int longestLen = 0;
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+        if(sum == 0) longestLen = i + 1;
+        else if(mpp.find(sum) != mpp.end()) {
+            longestLen = max(longestLen, i - mpp[sum]);
+        }
+        else {
+            mpp[sum] = i;
+        }
+    }
+    return longestLen;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << longest_length(arr, n) << '\n';
+}  
