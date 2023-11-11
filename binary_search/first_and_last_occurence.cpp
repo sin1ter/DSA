fd47e0ab76f68d4a7e60b326a57f4483edd50c1c
Symon <its.symonbarua@gmail.com>
2023-11-11 23:22:40 +0600

    binary_search

--- binary_search/first_and_last_occurence.cpp
+++ binary_search/first_and_last_occurence.cpp
@@ -41,8 +41,46 @@ int lastOccurence(vector<int>& arr, int n, int x) {
     // fascinating isn't it?
 }   
 
+// without lower bound and upper bound 
+int firstOcc(vector<int>& nums, int n, int target) {
+    int left = 0;
+    int right = n - 1;
+    int ans = -1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target) {
+            ans = mid;
+            right = mid - 1;
+        }
+        else if(nums[mid] > target) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+}
 
-
+int lastOcc(vector<int>& nums, int n, int target) {
+    int left = 0;
+    int right = n - 1;
+    int ans = -1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target) {
+            ans = mid;
+            left = mid + 1;
+        }
+        else if(nums[mid] > target) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+}
 int main() {
     int n, x;
     cin >> n >> x;
@@ -50,11 +88,18 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    int lb = firstOccurence(arr, n, x);
-    int ub = lastOccurence(arr, n, x);
-    if(lb == n or arr[lb] != x) lb = -1, ub = -1;
+    int lb = firstOcc(arr, n, x);
+    int ub = lastOcc(arr, n, x);
+    /*
+    for lower bound and upper bound
+    if(lb == n or arr[lb] != x) lb = -1, ub = -1; // for this time complexity is O(2*logn) and space complexity is O(n).
     else {
         cout << lb << " " << ub - 1 << '\n';
     }
+    */
+   if(lb == -1) cout << -1 << " " << -1 << '\n'; // because if the first occurence is not there there will not be last occurence.
+   else {
+    cout << lb << ' ' << ub << '\n';
+   }
         return 0;
 }
