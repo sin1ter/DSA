80f68ca54bbda0fcc887d0bbd8fb439cbf73a7cb
Symon <its.symonbarua@gmail.com>
2023-11-15 21:48:25 +0600

    binary_search

--- binary_search/search_single_element_sorted_array.cpp
+++ binary_search/search_single_element_sorted_array.cpp
@@ -13,8 +13,8 @@ int search_using_binarySearch(vector<int>& arr, int n) {
     if(n == 1) return arr[0];
     if(arr[0]!= arr[1])  return arr[0];
     if(arr[n - 1] != arr[n - 2]) return arr[n - 1];
-    int left = 0;
-    int right = n - 1;
+    int left = 1; // not taking left = 0 cause checking the first elment manually.
+    int right = n - 2; // as for the last element chekcing the last element manually so right = n - 2
     while(left <= right) {
         int mid = left + (right - left) / 2;
         //if arr[mid] is the single element.
@@ -31,7 +31,7 @@ int search_using_binarySearch(vector<int>& arr, int n) {
             right = mid - 1;
         }
     }
-    // as always remember to return dummy statement
+    // as always remember to return dummy statemen
     return -1;
 }
 
