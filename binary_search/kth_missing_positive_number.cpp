9fd1343afcb219a1c80dc10ca989d90fefcf4e7e
Symon <its.symonbarua@gmail.com>
2023-12-02 20:07:06 +0600

    binary_search

---
+++ binary_search/kth_missing_positive_number.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kth_brute(vector<int> arr, int n, int k) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] <= k) {
+            k++;
+        }
+        else break;
+    }
+    return k;
+}
+
+int kth_missing_number(vector<int> arr, int n, int k) {
+    int low = 0;
+    int high = n - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if(missing < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return k + high + 1;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // cout << kth_brute(arr, n, k) << endl;
+    cout << kth_missing_number(arr, n, k) << endl;
+        return 0;
+}
